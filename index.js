7520974205df4aa753979a21e893e489b056da02
Robert Sweet <robertsweetjr@gmail.com>
2021-02-01 16:05:17 -0500

    final mvp fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors    = require('cors')
 const scrapeReddit  = require('./data/reddit.js')
 const routes = require('./Routes/routes')
+const path = require('path')
 require('dotenv').config()
 
 const port = process.env.PORT || 5000
@@ -10,6 +11,10 @@ const app = express()
 
 app.use(cors())
 app.use('/api', routes)
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
 app.listen(port, () => {
     console.log(`Server running on ${port}`)
     scrapeReddit()
